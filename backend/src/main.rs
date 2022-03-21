85fb155e98b55e762fefe331a62cdfe6c0088ceb
Benjamin Morrell <BDMorrell@gmail.com>
2022-03-21 14:13:14 -0600

    Play around with routing

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use std::path::Path;
+use warp::http::{header::HeaderMap, Method};
+use warp::hyper::body::Bytes;
+use warp::path::FullPath;
 use warp::{self, filters, Filter};
 
 #[tokio::main]
@@ -17,12 +20,45 @@ async fn main() {
 
     log::info!("Configuration: {:?}", config);
 
-    let api = filters::path::path("api");
-    let api_reply = api.map(|| "This is api.\n");
+    let hello = filters::path::path("hello")
+        .and(filters::method::get())
+        .map(|| {"Hello, world!"});
+    const ECHO_BODY_LENGTH_LIMIT: u64 = 1024 * 8;
+    let echo = filters::path::path("echo")
+        // .and(filters::header::optional("name"))
+        // .and(filters::body::content_length_limit(ECHO_BODY_LENGTH_LIMIT))
+        .and(filters::body::bytes())
+        .and(filters::method::method())
+        .and(filters::path::full())
+        .and(filters::header::headers_cloned())
+        .map(
+            move |body: Bytes, method: Method, path: FullPath, headers: HeaderMap| {
+                let first_line = format!("{} {}", method, path.as_str());
+                let mut heads = String::new();
+                for (name, value) in headers.into_iter() {
+                    let name = match name {
+                        Some(header) => String::from(header.as_str()),
+                        None => String::from(""),
+                    };
+                    let header_string =
+                        format!("{}: {}\n", name, String::from_utf8_lossy(value.as_bytes()));
+                    heads.push_str(&header_string);
+                }
+                format!(
+                    "{}\n{}\n{}",
+                    first_line,
+                    heads,
+                    String::from_utf8_lossy(&body)
+                )
+            },
+        )
+        .with(filters::reply::header("Content-Type", "text/plain"));
+
+    let api = filters::path::path("api").and(echo.or(hello));
     let static_files = config.make_static_file_filter();
 
-    let filter = api_reply.or(static_files);
+    let routes = api.or(static_files);
 
-    let server = warp::serve(filter).run(socket);
+    let server = warp::serve(routes).run(socket);
     server.await
 }
