85a9c500cb31b9111a3cbe1d64c9dd760c73e515
Benjamin Morrell <BDMorrell@gmail.com>
2022-03-21 23:33:51 -0600

    Get the echo service running

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,25 @@
-use tokio::runtime;
+use chat_backend::api;
+use chat_backend::config;
+use hyper::server::Server;
+use hyper::service::make_service_fn;
+use std::convert::Infallible;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
+use tower::service_fn;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     chat_backend::init_log(false);
-    let configuration = chat_backend::config::get_configuration_from_current_directory();
-    runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(chat_backend::warp_server::serve_warp());
+
+    let config = config::get_configuration_from_current_directory()
+        .expect("Could not load server configuration");
+
+    let socket = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), config.port());
+
+    let service = make_service_fn(|_| async { Ok::<_, Infallible>(service_fn(api::echo)) });
+
+    let server = Server::bind(&socket).serve(service);
+
+    if let Err(e) = server.await {
+        eprintln!("Error: {}", e);
+    }
 }
