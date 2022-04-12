c2ac5d98aeaa0483344b07ceb361779c611f4a35
Benjamin Morrell <BDMorrell@gmail.com>
2022-04-12 01:57:55 -0600

    Start getting api/echo ready to go back into warp
    ...because now I know that I can do such things with `hyper::Body`.
    
    Also, the commit diff is particularly not helpful here, I think.

--- chat-server/src/api/echo.rs
+++ chat-server/src/api/echo.rs
@@ -7,21 +7,42 @@ use std::pin::Pin;
 use tokio::sync::mpsc::{self, Sender};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::{Stream, StreamExt};
+// use warp::{filters, Filter};
 
 type AsyncError = Box<dyn StdError + Send + Sync + 'static>;
 type AsyncResult = Result<bytes::Bytes, AsyncError>;
 type IntoBody = Box<(dyn Stream<Item = AsyncResult> + Send + 'static)>;
 
+// pub fn make_filter() -> impl Filter {
+//     filters::body::stream()
+//         .and(filters::method::method())
+//         .and(filters::path::full())
+//         .and(filters::header::headers_cloned())
+//         .map(|body, method, path, headers| {
+//             let first_line: Bytes = format!("{} {}", method, path).into();
+//             echo_service(first_line, &headers, Box::pin(body))
+//         })
+// }
+
 pub async fn echo(req: Request<Body>) -> Result<Response<Body>, HttpError> {
+    let first_line: Bytes =
+        format!("{} {} {:?}\r\n", req.method(), req.uri(), req.version()).into();
+    let headers = req.headers().clone();
+    let body = Box::pin(req.into_body());
+    echo_service(first_line, headers, body).await
+}
+
+async fn echo_service(
+    first_line: Bytes,
+    headers: HeaderMap,
+    body: Pin<Box<Body>>,
+) -> Result<Response<Body>, HttpError> {
     let (writer, response) = make_streaming_body(16);
 
     tokio::spawn(async move {
-        let first_line = format!("{} {} {:?}\r\n", req.method(), req.uri(), req.version());
-        writer.send(Ok(first_line.into())).await.unwrap();
-
-        echo_headers(req.headers(), &writer).await;
-
-        echo_body(Box::pin(req.into_body()), &writer).await;
+        writer.send(Ok(first_line)).await.unwrap();
+        echo_headers(headers, &writer).await;
+        echo_body(body, &writer).await;
     });
 
     Response::builder()
@@ -37,7 +58,7 @@ fn make_streaming_body(buffer: usize) -> (Sender<AsyncResult>, Body) {
     (writer, response)
 }
 
-async fn echo_headers(headers: &HeaderMap, destination: &Sender<AsyncResult>) {
+async fn echo_headers(headers: HeaderMap, destination: &Sender<AsyncResult>) {
     for (key, value) in headers.iter() {
         destination
             .send(Ok(format!("{:?} {:?}\r\n", key, value).into()))
