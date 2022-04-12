274710038a504ba2045f83edb88e08246a1fb3bd
Benjamin Morrell <BDMorrell@gmail.com>
2022-04-12 01:22:58 -0600

    Refactor apart api/echo

--- chat-server/src/api/echo.rs
+++ chat-server/src/api/echo.rs
@@ -1,51 +1,57 @@
 use bytes::Bytes;
 use http::Error as HttpError;
-use http::{Request, Response};
-use hyper::body::{Body, HttpBody};
+use http::{HeaderMap, Request, Response};
+use hyper::body::Body;
 use std::error::Error as StdError;
-use tokio::sync::mpsc;
+use std::pin::Pin;
+use tokio::sync::mpsc::{self, Sender};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_stream::{Stream, StreamExt};
-
-use log::info;
 
 type AsyncError = Box<dyn StdError + Send + Sync + 'static>;
 type AsyncResult = Result<bytes::Bytes, AsyncError>;
 type IntoBody = Box<(dyn Stream<Item = AsyncResult> + Send + 'static)>;
 
 pub async fn echo(req: Request<Body>) -> Result<Response<Body>, HttpError> {
-    // let (mut writer, responce_body) = Body::channel();
-    let (writer, response_stream) = mpsc::channel::<AsyncResult>(16);
-    let response_body: IntoBody = Box::new(ReceiverStream::new(response_stream));
-    let response = Body::from(response_body);
-
-    info!("response is {:?}", response);
+    let (writer, response) = make_streaming_body(16);
 
     tokio::spawn(async move {
         let first_line = format!("{} {} {:?}\r\n", req.method(), req.uri(), req.version());
         writer.send(Ok(first_line.into())).await.unwrap();
 
-        for (key, value) in req.headers().iter() {
-            writer
-                .send(Ok(format!("{:?} {:?}\r\n", key, value).into()))
-                .await
-                .unwrap();
-        }
-        writer
-            .send(Ok(Bytes::from(b"\r\n" as &'static [u8])))
-            .await
-            .unwrap();
+        echo_headers(req.headers(), &writer).await;
 
-        let mut body = Box::pin(req.into_body());
-        info!("request body is {:?}", body);
-
-        while let Some(Ok(data)) = body.next().await {
-            writer.send(Ok(data)).await.unwrap();
-        }
+        echo_body(Box::pin(req.into_body()), &writer).await;
     });
 
     Response::builder()
         .status(200)
         .header("Content-Type", "text/plain")
         .body(response)
+}
+
+fn make_streaming_body(buffer: usize) -> (Sender<AsyncResult>, Body) {
+    let (writer, response_stream) = mpsc::channel::<AsyncResult>(buffer);
+    let response_body: IntoBody = Box::new(ReceiverStream::new(response_stream));
+    let response = Body::from(response_body);
+    (writer, response)
+}
+
+async fn echo_headers(headers: &HeaderMap, destination: &Sender<AsyncResult>) {
+    for (key, value) in headers.iter() {
+        destination
+            .send(Ok(format!("{:?} {:?}\r\n", key, value).into()))
+            .await
+            .unwrap();
+    }
+    destination
+        .send(Ok(Bytes::from(b"\r\n" as &'static [u8])))
+        .await
+        .unwrap();
+}
+
+async fn echo_body(mut input_body: Pin<Box<Body>>, destination: &Sender<AsyncResult>) {
+    while let Some(Ok(data)) = input_body.next().await {
+        destination.send(Ok(data)).await.unwrap();
+    }
 }
