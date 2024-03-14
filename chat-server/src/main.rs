3d07df5e7b750a98c7d540d93952fc7bad9d357d
Benjamin Morrell <BDMorrell@gmail.com>
2024-03-14 14:17:43 -0600

    Break things and scaffold a chat service

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -3,10 +3,18 @@ use std::io::ErrorKind;
 use std::net::SocketAddr;
 use std::path::{Path, PathBuf};
 
-use axum::Router;
-use serde::Deserialize;
+use axum::extract::ws::{Message, WebSocket};
+use axum::extract::{ConnectInfo, WebSocketUpgrade};
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::{Json, Router};
+use axum_extra::{headers, TypedHeader};
+use serde::{Deserialize, Serialize};
+use tokio::sync::broadcast;
 use tower_http::services::ServeDir;
-use tracing::{info, Level};
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+use tracing::level_filters::LevelFilter;
+use tracing::{event, info, warn};
 use tracing_subscriber::EnvFilter;
 
 fn main() {
@@ -14,7 +22,7 @@ fn main() {
         .compact()
         .with_env_filter(
             EnvFilter::builder()
-                .with_default_directive(Level::INFO.into())
+                .with_default_directive(LevelFilter::DEBUG.into())
                 .from_env_lossy(),
         )
         .try_init()
@@ -33,7 +41,18 @@ async fn async_main(config: Config) {
         .await
         .expect("Could not start the socket listener.");
 
-    let router = Router::new().nest_service("/", ServeDir::new(&config.static_dir));
+    let notifyAllTx = broadcast::Sender::new(2048);
+
+    let router = Router::new()
+        .route(
+            "/api/ws",
+            get(|ws: WebSocketUpgrade| -> impl IntoResponse {}),
+        )
+        .nest_service("/", ServeDir::new(&config.static_dir))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
 
     info!("Listening for connections on {}.", config.socket);
 
@@ -66,4 +85,55 @@ pub fn default_load_config() -> Result<Config, Box<dyn std::error::Error>> {
 pub struct Config {
     pub static_dir: PathBuf,
     pub socket: SocketAddr,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Event {
+    Text(String),
+}
+
+#[derive(Debug)]
+pub struct WebsocketSharedState {
+    pub sender: broadcast::Sender<Event>,
+}
+
+#[derive(Debug)]
+pub struct SharedStateWebsocketHandler {
+    state: Arc<WebsocketSharedState>,
+}
+
+impl SharedStateWebsocketHandler {
+    pub fn new(state: Arc<WebsocketSharedState>) -> Self {
+        Self(state)
+    }
+
+    fn handle_connection(self: &mut Self, ws: WebSocketUpgrade) -> impl IntoResponse {
+        let new_sender = self.sender.clone();
+        ws.protocols("custom")
+            .on_failed_upgrade(|error| {
+                warn!("Websocket failed to upgrade: {}", error);
+            })
+            .on_upgrade(move |socket| {
+                let tx = new_sender;
+                let rx = tx.subscribe();
+                let (sender, receiver) = socket.split();
+                tokio::spawn(handle_writing(sender, rx));
+                tokio::spawn(handle_reading(receiver, tx));
+            })
+    }
+
+    async fn handle_writing(sender: SplitStream<WebSocket>, event_source: broadcast::Receiver<Event>) {
+        while Some(event) = await event_source.next() {
+            write!(sender, event);
+        }
+    }
+
+    async fn handle_reading(reader: SplitSink<WebSocket, Message>, event_target: broadcast::Sender<Event>) {
+        while Some(text) = await reader.next() {
+            match Json::from_bytes(text) {
+                Some(message) => event_target.send(message),
+                Err(e) => warn!("Bad event from <anonymous>: {}", e),
+            }
+        }
+    }
 }
