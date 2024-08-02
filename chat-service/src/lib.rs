36be5db73e9d65cbf794133de4564602e2d1df59
Benjamin Morrell <BDMorrell@gmail.com>
2024-08-02 14:55:08 -0600

    Implement [chat-service]/get/all

--- chat-service/src/lib.rs
+++ chat-service/src/lib.rs
@@ -1,9 +1,10 @@
+use std::future::Future;
 use std::sync::Arc;
 
-use axum::{extract::{Request, State}, http::StatusCode, routing::get, Json, Router};
+use axum::extract::State;
+use axum::{http::StatusCode, routing, Json, Router};
 use chatroom::{Chatroom, Message};
-use tokio::sync::Mutex;
-use tower::Service;
+use tokio::sync::{Mutex, MutexGuard};
 
 #[derive(Debug, Clone)]
 pub struct ChatService {
@@ -11,23 +12,30 @@ pub struct ChatService {
 }
 
 impl ChatService {
-    pub fn with_room(chatroom: Arc<Mutex<Chatroom>>) -> Self{
-        Self {
-            chatroom,
-        }
+    pub fn with_room(chatroom: Arc<Mutex<Chatroom>>) -> Self {
+        Self { chatroom }
     }
 
-    pub fn router(&self) -> Router {
+    pub fn router(&self) -> Router<Self> {
         Router::new()
-        .route("/get/all", get(get_all))
-        .with_state(self)
+            .route("/get/all", routing::get(get_all))
+            .with_state(self.clone())
+    }
+
+    pub fn chatroom_lock(&self) -> impl Future<Output = MutexGuard<Chatroom>> {
+        self.chatroom.lock()
     }
 }
 
-async fn get_all(State(state): State<&ChatService>) -> (StatusCode, Json<Vec<Arc<Message>>>) {
-    let lock = state.chatroom.lock().await;
-    match lock.try_get_range(..) {
-        Some(value) => (StatusCode::CREATED, Json::from(value)),
-        None => (StatusCode::BAD_REQUEST, Json::from(vec![])),
+async fn get_all(State(state): State<ChatService>) -> (StatusCode, Json<Vec<Arc<Message>>>) {
+    let chat_state: Option<Vec<Arc<Message>>> = {
+        // Isolate the Mutex from any function errors and/or panics
+        let lock = state.chatroom_lock().await;
+        lock.try_get_range(..)
+    };
+
+    match chat_state {
+        Some(messages) => (StatusCode::OK, Json(messages)),
+        None => (StatusCode::SERVICE_UNAVAILABLE, Json([].into())),
     }
 }
